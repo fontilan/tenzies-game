894eb50cdb456ab84bc19a00e406ccd256737914
fontilan <68748054+fontilan@users.noreply.github.com>
2022-04-02 18:52:14 +0200

    add a lighter shadow to held die

--- src/Die.js
+++ src/Die.js
@@ -1,7 +1,9 @@
 function Die(props) {
   const styles = {
     backgroundColor: props.isHeld ? '#59E391' : '#FFFFFF',
-    boxShadow: props.isHeld ? 'none' : '0px 4px 4px rgba(0, 0, 0, 0.3)',
+    boxShadow: props.isHeld
+      ? '0px 2px 4px rgba(0, 0, 0, 0.3)'
+      : '0px 4px 4px rgba(0, 0, 0, 0.3)',
   };
 
   return (
