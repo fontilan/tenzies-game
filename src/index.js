a0063be7227df7bdd5310e110ad02f08271098ce
fontilan <68748054+fontilan@users.noreply.github.com>
2022-04-03 11:34:05 +0200

    add features to add

--- src/index.js
+++ src/index.js
@@ -6,3 +6,9 @@ import '../src/index.css';
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
+
+// TODO (FEATURES TO ADD)
+// 1. add real dots instead of numbers on the dice
+// 2. track the number of rolls
+// 3. track the time it took to win
+// 4. save best time to localStorage
