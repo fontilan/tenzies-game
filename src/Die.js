337fc0bb63a115218688c321431cab4d5bf6a230
fontilan <68748054+fontilan@users.noreply.github.com>
2022-08-29 22:41:28 +0200

    refactor: import the svgs directly

--- src/Die.js
+++ src/Die.js
@@ -1,10 +1,3 @@
-import one from './images/1.svg';
-import two from './images/2.svg';
-import three from './images/3.svg';
-import four from './images/4.svg';
-import five from './images/5.svg';
-import six from './images/6.svg';
-
 function Die({ isHeld, value, holdDice }) {
   const styles = {
     fill: isHeld ? '#59E391' : '#FFFFFF',
@@ -12,6 +5,84 @@ function Die({ isHeld, value, holdDice }) {
       ? '0px 2px 4px rgba(0, 0, 0, 0.3)'
       : '0px 4px 4px rgba(0, 0, 0, 0.3)',
   };
+
+  let one = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <path d="M0 0h512v512H0z" fill="#000" fill-opacity="1"></path>
+      <g class="" transform="translate(0,0)">
+        <path
+          d="M74.5 36A38.5 38.5 0 0 0 36 74.5v363A38.5 38.5 0 0 0 74.5 476h363a38.5 38.5 0 0 0 38.5-38.5v-363A38.5 38.5 0 0 0 437.5 36h-363zM256 206a50 50 0 0 1 0 100 50 50 0 0 1 0-100z"
+          fill="#fff"
+          fill-opacity="1"
+        ></path>
+      </g>
+    </svg>
+  );
+
+  let two = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <path d="M0 0h512v512H0z" fill="#000" fill-opacity="1"></path>
+      <g class="" transform="translate(0,0)">
+        <path
+          d="M74.5 36A38.5 38.5 0 0 0 36 74.5v363A38.5 38.5 0 0 0 74.5 476h363a38.5 38.5 0 0 0 38.5-38.5v-363A38.5 38.5 0 0 0 437.5 36h-363zm316.97 36.03A50 50 0 0 1 440 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm-268 268A50 50 0 0 1 172 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97z"
+          fill="#fff"
+          fill-opacity="1"
+        ></path>
+      </g>
+    </svg>
+  );
+
+  let three = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <path d="M0 0h512v512H0z" fill="#000" fill-opacity="1"></path>
+      <g class="" transform="translate(0,0)">
+        <path
+          d="M74.5 36A38.5 38.5 0 0 0 36 74.5v363A38.5 38.5 0 0 0 74.5 476h363a38.5 38.5 0 0 0 38.5-38.5v-363A38.5 38.5 0 0 0 437.5 36h-363zm316.97 36.03A50 50 0 0 1 440 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zM256 206a50 50 0 0 1 0 100 50 50 0 0 1 0-100zM123.47 340.03A50 50 0 0 1 172 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97z"
+          fill="#fff"
+          fill-opacity="1"
+        ></path>
+      </g>
+    </svg>
+  );
+
+  let four = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <path d="M0 0h512v512H0z" fill="#000" fill-opacity="1"></path>
+      <g class="" transform="translate(0,0)">
+        <path
+          d="M74.5 36A38.5 38.5 0 0 0 36 74.5v363A38.5 38.5 0 0 0 74.5 476h363a38.5 38.5 0 0 0 38.5-38.5v-363A38.5 38.5 0 0 0 437.5 36h-363zm48.97 36.03A50 50 0 0 1 172 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm268 0A50 50 0 0 1 440 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm-268 268A50 50 0 0 1 172 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm268 0A50 50 0 0 1 440 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97z"
+          fill="#fff"
+          fill-opacity="1"
+        ></path>
+      </g>
+    </svg>
+  );
+
+  let five = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <path d="M0 0h512v512H0z" fill="#000" fill-opacity="1"></path>
+      <g class="" transform="translate(0,0)">
+        <path
+          d="M74.5 36A38.5 38.5 0 0 0 36 74.5v363A38.5 38.5 0 0 0 74.5 476h363a38.5 38.5 0 0 0 38.5-38.5v-363A38.5 38.5 0 0 0 437.5 36h-363zm48.97 36.03A50 50 0 0 1 172 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm268 0A50 50 0 0 1 440 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zM256 206a50 50 0 0 1 0 100 50 50 0 0 1 0-100zM123.47 340.03A50 50 0 0 1 172 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm268 0A50 50 0 0 1 440 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97z"
+          fill="#fff"
+          fill-opacity="1"
+        ></path>
+      </g>
+    </svg>
+  );
+
+  let six = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <path d="M0 0h512v512H0z" fill="#000" fill-opacity="1"></path>
+      <g class="" transform="translate(0,0)">
+        <path
+          d="M74.5 36A38.5 38.5 0 0 0 36 74.5v363A38.5 38.5 0 0 0 74.5 476h363a38.5 38.5 0 0 0 38.5-38.5v-363A38.5 38.5 0 0 0 437.5 36h-363zm48.97 36.03A50 50 0 0 1 172 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm268 0A50 50 0 0 1 440 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zM122 206a50 50 0 0 1 0 100 50 50 0 0 1 0-100zm268 0a50 50 0 0 1 0 100 50 50 0 0 1 0-100zM123.47 340.03A50 50 0 0 1 172 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm268 0A50 50 0 0 1 440 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97z"
+          fill="#fff"
+          fill-opacity="1"
+        ></path>
+      </g>
+    </svg>
+  );
 
   let diceImage = '';
 
@@ -37,7 +108,8 @@ function Die({ isHeld, value, holdDice }) {
 
   return (
     <button style={styles} className="dice" onClick={holdDice}>
-      <img src={diceImage} alt={value} />
+      {diceImage}
+      {/* <img src={diceImage} alt={value} /> */}
     </button>
   );
 }
