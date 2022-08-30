261d2eb1f65b8030bf789d4c63372f2bbb746fc5
fontilan <68748054+fontilan@users.noreply.github.com>
2022-08-30 08:34:16 +0200

    fix: svg properties names not being camelCase

--- src/Die.js
+++ src/Die.js
@@ -2,12 +2,12 @@ function Die({ isHeld, value, holdDice }) {
   // each die is specified as a separate svg that has a fill property based on the isHeld prop, so that it turns green when it is held by the player
   let one = (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-      <path d="M0 0h512v512H0z" fill="#000" fill-opacity="1"></path>
-      <g class="" transform="translate(0,0)">
+      <path d="M0 0h512v512H0z" fill="#000" fillOpacity="1"></path>
+      <g className="" transform="translate(0,0)">
         <path
           d="M74.5 36A38.5 38.5 0 0 0 36 74.5v363A38.5 38.5 0 0 0 74.5 476h363a38.5 38.5 0 0 0 38.5-38.5v-363A38.5 38.5 0 0 0 437.5 36h-363zM256 206a50 50 0 0 1 0 100 50 50 0 0 1 0-100z"
           fill={isHeld ? '#59E391' : '#FFFFFF'}
-          fill-opacity="1"
+          fillOpacity="1"
         ></path>
       </g>
     </svg>
@@ -15,12 +15,12 @@ function Die({ isHeld, value, holdDice }) {
 
   let two = (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-      <path d="M0 0h512v512H0z" fill="#000" fill-opacity="1"></path>
-      <g class="" transform="translate(0,0)">
+      <path d="M0 0h512v512H0z" fill="#000" fillOpacity="1"></path>
+      <g className="" transform="translate(0,0)">
         <path
           d="M74.5 36A38.5 38.5 0 0 0 36 74.5v363A38.5 38.5 0 0 0 74.5 476h363a38.5 38.5 0 0 0 38.5-38.5v-363A38.5 38.5 0 0 0 437.5 36h-363zm316.97 36.03A50 50 0 0 1 440 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm-268 268A50 50 0 0 1 172 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97z"
           fill={isHeld ? '#59E391' : '#FFFFFF'}
-          fill-opacity="1"
+          fillOpacity="1"
         ></path>
       </g>
     </svg>
@@ -28,12 +28,12 @@ function Die({ isHeld, value, holdDice }) {
 
   let three = (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-      <path d="M0 0h512v512H0z" fill="#000" fill-opacity="1"></path>
-      <g class="" transform="translate(0,0)">
+      <path d="M0 0h512v512H0z" fill="#000" fillOpacity="1"></path>
+      <g className="" transform="translate(0,0)">
         <path
           d="M74.5 36A38.5 38.5 0 0 0 36 74.5v363A38.5 38.5 0 0 0 74.5 476h363a38.5 38.5 0 0 0 38.5-38.5v-363A38.5 38.5 0 0 0 437.5 36h-363zm316.97 36.03A50 50 0 0 1 440 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zM256 206a50 50 0 0 1 0 100 50 50 0 0 1 0-100zM123.47 340.03A50 50 0 0 1 172 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97z"
           fill={isHeld ? '#59E391' : '#FFFFFF'}
-          fill-opacity="1"
+          fillOpacity="1"
         ></path>
       </g>
     </svg>
@@ -41,12 +41,12 @@ function Die({ isHeld, value, holdDice }) {
 
   let four = (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-      <path d="M0 0h512v512H0z" fill="#000" fill-opacity="1"></path>
-      <g class="" transform="translate(0,0)">
+      <path d="M0 0h512v512H0z" fill="#000" fillOpacity="1"></path>
+      <g className="" transform="translate(0,0)">
         <path
           d="M74.5 36A38.5 38.5 0 0 0 36 74.5v363A38.5 38.5 0 0 0 74.5 476h363a38.5 38.5 0 0 0 38.5-38.5v-363A38.5 38.5 0 0 0 437.5 36h-363zm48.97 36.03A50 50 0 0 1 172 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm268 0A50 50 0 0 1 440 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm-268 268A50 50 0 0 1 172 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm268 0A50 50 0 0 1 440 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97z"
           fill={isHeld ? '#59E391' : '#FFFFFF'}
-          fill-opacity="1"
+          fillOpacity="1"
         ></path>
       </g>
     </svg>
@@ -54,12 +54,12 @@ function Die({ isHeld, value, holdDice }) {
 
   let five = (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-      <path d="M0 0h512v512H0z" fill="#000" fill-opacity="1"></path>
-      <g class="" transform="translate(0,0)">
+      <path d="M0 0h512v512H0z" fill="#000" fillOpacity="1"></path>
+      <g className="" transform="translate(0,0)">
         <path
           d="M74.5 36A38.5 38.5 0 0 0 36 74.5v363A38.5 38.5 0 0 0 74.5 476h363a38.5 38.5 0 0 0 38.5-38.5v-363A38.5 38.5 0 0 0 437.5 36h-363zm48.97 36.03A50 50 0 0 1 172 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm268 0A50 50 0 0 1 440 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zM256 206a50 50 0 0 1 0 100 50 50 0 0 1 0-100zM123.47 340.03A50 50 0 0 1 172 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm268 0A50 50 0 0 1 440 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97z"
           fill={isHeld ? '#59E391' : '#FFFFFF'}
-          fill-opacity="1"
+          fillOpacity="1"
         ></path>
       </g>
     </svg>
@@ -67,12 +67,12 @@ function Die({ isHeld, value, holdDice }) {
 
   let six = (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-      <path d="M0 0h512v512H0z" fill="#000" fill-opacity="1"></path>
-      <g class="" transform="translate(0,0)">
+      <path d="M0 0h512v512H0z" fill="#000" fillOpacity="1"></path>
+      <g className="" transform="translate(0,0)">
         <path
           d="M74.5 36A38.5 38.5 0 0 0 36 74.5v363A38.5 38.5 0 0 0 74.5 476h363a38.5 38.5 0 0 0 38.5-38.5v-363A38.5 38.5 0 0 0 437.5 36h-363zm48.97 36.03A50 50 0 0 1 172 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm268 0A50 50 0 0 1 440 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zM122 206a50 50 0 0 1 0 100 50 50 0 0 1 0-100zm268 0a50 50 0 0 1 0 100 50 50 0 0 1 0-100zM123.47 340.03A50 50 0 0 1 172 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm268 0A50 50 0 0 1 440 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97z"
           fill={isHeld ? '#59E391' : '#FFFFFF'}
-          fill-opacity="1"
+          fillOpacity="1"
         ></path>
       </g>
     </svg>
