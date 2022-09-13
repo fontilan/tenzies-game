e8704ab2ab8b57daa80ff43e73f1686a01fe5b02
fontilan <68748054+fontilan@users.noreply.github.com>
2022-09-13 14:04:47 +0200

    feat: give each die a unique color when held

--- src/Die.js
+++ src/Die.js
@@ -1,12 +1,14 @@
 function Die({ isHeld, value, holdDice }) {
   // each die is specified as a separate svg that has a fill property based on the isHeld prop, so that it turns green when it is held by the player
+  // each die has a unique hex code in the isHeld check - each dice has a unique color when held
+
   let one = (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
       <path d="M0 0h512v512H0z" fill="#000" fillOpacity="1"></path>
       <g className="" transform="translate(0,0)">
         <path
           d="M74.5 36A38.5 38.5 0 0 0 36 74.5v363A38.5 38.5 0 0 0 74.5 476h363a38.5 38.5 0 0 0 38.5-38.5v-363A38.5 38.5 0 0 0 437.5 36h-363zM256 206a50 50 0 0 1 0 100 50 50 0 0 1 0-100z"
-          fill={isHeld ? '#59E391' : '#FFFFFF'}
+          fill={isHeld ? '#f2cdcd' : '#FFFFFF'}
           fillOpacity="1"
         ></path>
       </g>
@@ -19,7 +21,7 @@ function Die({ isHeld, value, holdDice }) {
       <g className="" transform="translate(0,0)">
         <path
           d="M74.5 36A38.5 38.5 0 0 0 36 74.5v363A38.5 38.5 0 0 0 74.5 476h363a38.5 38.5 0 0 0 38.5-38.5v-363A38.5 38.5 0 0 0 437.5 36h-363zm316.97 36.03A50 50 0 0 1 440 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm-268 268A50 50 0 0 1 172 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97z"
-          fill={isHeld ? '#59E391' : '#FFFFFF'}
+          fill={isHeld ? '#ddb6f2' : '#FFFFFF'}
           fillOpacity="1"
         ></path>
       </g>
@@ -32,7 +34,7 @@ function Die({ isHeld, value, holdDice }) {
       <g className="" transform="translate(0,0)">
         <path
           d="M74.5 36A38.5 38.5 0 0 0 36 74.5v363A38.5 38.5 0 0 0 74.5 476h363a38.5 38.5 0 0 0 38.5-38.5v-363A38.5 38.5 0 0 0 437.5 36h-363zm316.97 36.03A50 50 0 0 1 440 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zM256 206a50 50 0 0 1 0 100 50 50 0 0 1 0-100zM123.47 340.03A50 50 0 0 1 172 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97z"
-          fill={isHeld ? '#59E391' : '#FFFFFF'}
+          fill={isHeld ? '#f8bd96' : '#FFFFFF'}
           fillOpacity="1"
         ></path>
       </g>
@@ -45,7 +47,7 @@ function Die({ isHeld, value, holdDice }) {
       <g className="" transform="translate(0,0)">
         <path
           d="M74.5 36A38.5 38.5 0 0 0 36 74.5v363A38.5 38.5 0 0 0 74.5 476h363a38.5 38.5 0 0 0 38.5-38.5v-363A38.5 38.5 0 0 0 437.5 36h-363zm48.97 36.03A50 50 0 0 1 172 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm268 0A50 50 0 0 1 440 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm-268 268A50 50 0 0 1 172 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm268 0A50 50 0 0 1 440 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97z"
-          fill={isHeld ? '#59E391' : '#FFFFFF'}
+          fill={isHeld ? '#fae3b0' : '#FFFFFF'}
           fillOpacity="1"
         ></path>
       </g>
@@ -58,7 +60,7 @@ function Die({ isHeld, value, holdDice }) {
       <g className="" transform="translate(0,0)">
         <path
           d="M74.5 36A38.5 38.5 0 0 0 36 74.5v363A38.5 38.5 0 0 0 74.5 476h363a38.5 38.5 0 0 0 38.5-38.5v-363A38.5 38.5 0 0 0 437.5 36h-363zm48.97 36.03A50 50 0 0 1 172 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm268 0A50 50 0 0 1 440 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zM256 206a50 50 0 0 1 0 100 50 50 0 0 1 0-100zM123.47 340.03A50 50 0 0 1 172 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm268 0A50 50 0 0 1 440 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97z"
-          fill={isHeld ? '#59E391' : '#FFFFFF'}
+          fill={isHeld ? '#abe9b3' : '#FFFFFF'}
           fillOpacity="1"
         ></path>
       </g>
@@ -71,7 +73,7 @@ function Die({ isHeld, value, holdDice }) {
       <g className="" transform="translate(0,0)">
         <path
           d="M74.5 36A38.5 38.5 0 0 0 36 74.5v363A38.5 38.5 0 0 0 74.5 476h363a38.5 38.5 0 0 0 38.5-38.5v-363A38.5 38.5 0 0 0 437.5 36h-363zm48.97 36.03A50 50 0 0 1 172 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm268 0A50 50 0 0 1 440 122a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zM122 206a50 50 0 0 1 0 100 50 50 0 0 1 0-100zm268 0a50 50 0 0 1 0 100 50 50 0 0 1 0-100zM123.47 340.03A50 50 0 0 1 172 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97zm268 0A50 50 0 0 1 440 390a50 50 0 0 1-100 0 50 50 0 0 1 51.47-49.97z"
-          fill={isHeld ? '#59E391' : '#FFFFFF'}
+          fill={isHeld ? '#96cdfb' : '#FFFFFF'}
           fillOpacity="1"
         ></path>
       </g>
