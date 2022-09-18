00428c0fceb27e5181aff41e146b6cf8e84c256a
fontilan <68748054+fontilan@users.noreply.github.com>
2022-09-18 14:29:37 +0200

    fix: (ESLint) change file extension to .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
 
+import Confetti from 'react-confetti';
+import { nanoid } from 'nanoid';
 import DiceContainer from './components/DiceContainer';
 import GameText from './components/GameText';
 import Header from './components/Header';
-
-import Confetti from 'react-confetti';
-import { nanoid } from 'nanoid';
 
 function App() {
   // get the values of best roll and best time from local storage. If it's not there yet then set it to null
@@ -57,8 +56,8 @@ function App() {
 
   // generate 10 new dice
   function allNewDice() {
-    let newDice = [];
-    for (let i = 0; i < 10; i++) {
+    const newDice = [];
+    for (let i = 0; i < 10; i += 1) {
       newDice.push(generateDice());
     }
     return newDice;
@@ -78,8 +77,8 @@ function App() {
   // do the same with the number of rolls
   function endGame() {
     setTenzies(true);
-    let endTimer = new Date();
-    let timeDifference = endTimer.getTime() - startTimer.getTime();
+    const endTimer = new Date();
+    const timeDifference = endTimer.getTime() - startTimer.getTime();
     setGameTime(timeDifference);
     if (bestTime === null || bestTime > timeDifference) {
       setBestTime(timeDifference);
@@ -94,9 +93,7 @@ function App() {
   function rollDice() {
     if (!tenzies) {
       setDice((oldDice) =>
-        oldDice.map((die) => {
-          return die.isHeld ? die : generateDice();
-        }),
+        oldDice.map((die) => (die.isHeld ? die : generateDice())),
       );
       setNumOfRolls((prevNum) => prevNum + 1);
     } else {
@@ -107,16 +104,16 @@ function App() {
   // hold the dice - map through the current dice array (oldDice), find the one to hold by the id that is passed when triggering this funcion, set its isHeld prop accordingly, from true to false and vice versa
   function holdDice(id) {
     setDice((oldDice) =>
-      oldDice.map((die) => {
-        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
-      }),
+      oldDice.map((die) =>
+        die.id === id ? { ...die, isHeld: !die.isHeld } : die,
+      ),
     );
   }
 
   // the confetti is thrown when the user wins the game
   // the button changes its text based on whether or not the game is finished (tenzies is true)
   return (
-    <main className="main">
+    <main className='main'>
       {tenzies && <Confetti />}
       <Header tenzies={tenzies} />
       <GameText
@@ -126,9 +123,9 @@ function App() {
         numOfRolls={numOfRolls}
         tenzies={tenzies}
       />
-      <DiceContainer dice={dice} holdDice={holdDice} />
+      <DiceContainer dice={dice} holdDice={holdDice()} />
       <div>
-        <button onClick={rollDice} className="roll-button">
+        <button type='button' onClick={rollDice} className='roll-button'>
           {tenzies ? 'New Game' : 'Roll'}
         </button>
       </div>
