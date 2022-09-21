d1dcb37020dc784722dbf699bdcf8a9b9109473c
fontilan <68748054+fontilan@users.noreply.github.com>
2022-09-21 09:54:16 +0200

    refactor: move holdDice function here

--- src/components/DiceContainer.jsx
+++ src/components/DiceContainer.jsx
@@ -1,8 +1,17 @@
 import PropTypes from 'prop-types';
 import Die from './Die';
 
-// assign each object from the dice array (each die) to a Die component
-function DiceContainer({ dice, holdDice }) {
+function DiceContainer({ dice, setDice }) {
+  // hold the dice - map through the current dice array (oldDice), find the one to hold by the id that is passed when triggering this funcion, set its isHeld prop accordingly, from true to false and vice versa
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) =>
+        die.id === id ? { ...die, isHeld: !die.isHeld } : die,
+      ),
+    );
+  }
+
+  // assign each object from the dice array (each die) to a Die component
   return (
     <div className="dice-container">
       {dice.map((die) => (
@@ -19,7 +28,7 @@ function DiceContainer({ dice, holdDice }) {
 
 DiceContainer.propTypes = {
   dice: PropTypes.arrayOf(PropTypes.objectOf).isRequired,
-  holdDice: PropTypes.func.isRequired,
+  setDice: PropTypes.func.isRequired,
 };
 
 export default DiceContainer;
